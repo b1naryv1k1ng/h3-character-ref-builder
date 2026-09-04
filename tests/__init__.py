8bedf5ec7bc91e7196a87a7756203921b292eedf
b1naryyv1k1ng <mobrien86@gmail.com>
2026-09-04 14:05:29 -0500

    Stop tracking local tests

--- tests/__init__.py
+++
@@ -1,1 +0,0 @@
-"""H3 Character Ref Builder test suite."""
--- tests/conftest.py
+++
@@ -1,8 +0,0 @@
-import pytest
-
-from h3_character_ref_builder.character_store import CharacterStore
-
-
-@pytest.fixture
-def store(tmp_path):
-    return CharacterStore(tmp_path / "managed")
